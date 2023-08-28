2315b8c2b018193548ad90303d93e72851f56ced
Yusuke Kawamoto <novogrammer@gmail.com>
2023-08-28 16:54:23 +0900

    クライアント越しの画像保存

--- server/index.mjs
+++ server/index.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import next from "next";
+import fs from "fs/promises";
 
 const dev = process.env.NODE_ENV !== 'production';
 const port = 3000;
@@ -10,26 +11,69 @@ const sleepAsync=(t)=>new Promise((resolve)=>{
   setTimeout(resolve,t*1000);
 });
 
+const INPUT_IMAGE="./input.jpg";
+const OUTPUT_IMAGE="./output.jpg";
+
 (async () => {
   try {
     await app.prepare();
     const server = express();
+    server.use(express.json())
+
+    const bufferInputImage=await fs.readFile(INPUT_IMAGE);
+    const base64InputImage=`data:image/jpeg;base64,${bufferInputImage.toString("base64")}`;
+
     const q=[];
-    server.get("/add",(req,res)=>{
-      const t=performance.now();
-      q.push(t);
-      res.send("added");
-    })
+    setInterval(()=>{
+      if(q.length==0){
+        q.push(base64InputImage);
+      }
+    },1000*10);
+    // server.get("/add",(req,res)=>{
+    //   q.push(base64InputImage);
+    //   res.send("added");
+    // })
+    server.post("/set",(req,res)=>{
+      console.log('setup: data', req.body.data);
+      let base64OutputImage=req.body.data;
+      if(typeof req.body.data !=="string"){
+        res.json({
+          result:"error",
+          message:"data is not string",
+        });
+        return;
+      }
+      base64OutputImage=base64OutputImage.replace("data:image/jpeg;base64","");
+      const bufferOutputImage=Buffer.from(base64OutputImage,"base64");
+      fs.writeFile(OUTPUT_IMAGE,bufferOutputImage).then(()=>{
+        res.json({
+          result:"ok",
+        });
+      }).catch((error)=>{
+        if(error instanceof Error){
+          res.json({
+            result:"error",
+            message:error.message,
+          });
+        }else{
+          res.json({
+            result:"error",
+            message:"unknown",
+          });
+        }
+      })
+    });
+
     server.get("/get",async (req,res)=>{
       const beginTime=performance.now();
       try{
         let currentTime=performance.now();
         while(currentTime-beginTime<5*1000){
-          const t=q.shift();
-          if(t!==undefined){
+          const base64Image=q.shift();
+          if(base64Image!==undefined){
             res.json({
               result:"ok",
-              t:t,
+              data:base64Image,
             });
             return;
           }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,95 @@
+'use client'
+/* eslint-disable @next/next/no-img-element */
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useCallback, useState,MouseEvent } from 'react'
+
+interface GetBodyOk{
+  result:"ok";
+  data:string;
+}
+interface GetBodyTimeout{
+  result:"timeout";
+
+}
+interface GetBodyError{
+  result:"error";
+  message:string;
+}
+
+type GetBody=GetBodyOk|GetBodyTimeout|GetBodyError;
 
 export default function Home() {
+
+  const [image,setImage]=useState<string|undefined>(undefined);
+
+  // useEffect(()=>{
+
+  // },[]);
+  const onGetImage=useCallback((event:MouseEvent<HTMLButtonElement>)=>{
+    // event.preventDefault();
+    // event.stopPropagation();
+    fetch("/get").then((response)=>response.json()).then((json:GetBody)=>{
+      switch(json.result){
+        case "ok":
+          console.log(json.data);
+          setImage((_previousImage)=>{
+            return json.data;
+          })
+          break;
+        case "timeout":
+          console.log("timeout");
+          break;
+        case "error":
+          console.log(`error message:${json.message}`)
+          break;
+        default:
+          console.log("unknown result");
+          break;
+      }
+    }).catch((_error)=>{
+
+      console.log("error");
+    })
+  },[]);
+  const onSetImage=useCallback((event:MouseEvent<HTMLButtonElement>)=>{
+    // event.preventDefault();
+    // event.stopPropagation();
+    console.log("before set");
+    if(image===undefined){
+      console.log("skip");
+      return;
+    }
+    const body={
+      data:image,
+    };
+    fetch("/set",{
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json",
+      },
+      body:JSON.stringify(body),
+    }).then((response)=>response.json()).then((json)=>{
+      switch(json.result){
+        case "ok":
+          console.log("ok");
+          break;
+        default:
+          console.log("unknown result");
+          break;
+      }
+    });
+  },[image]);
+  
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main>
+      <div>
+        <button onClick={onGetImage}>get</button>
+        <button onClick={onSetImage}>set</button>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        <img src={image} alt="" />
       </div>
     </main>
   )
