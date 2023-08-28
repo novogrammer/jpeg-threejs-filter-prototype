4290171e83f963bd24a66453cb1fed66d5c808e0
Yusuke Kawamoto <novogrammer@gmail.com>
2023-08-28 17:20:33 +0900

    自動実行

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from 'next/image'
 import styles from './page.module.css'
-import { useCallback, useState,MouseEvent } from 'react'
+import { useCallback, useState,MouseEvent, useEffect } from 'react'
 
 interface GetBodyOk{
   result:"ok";
@@ -23,9 +23,51 @@ export default function Home() {
 
   const [image,setImage]=useState<string|undefined>(undefined);
 
-  // useEffect(()=>{
+  useEffect(()=>{
+    const main=async ()=>{
+      while(true){
+        const jsonGet:GetBody = await fetch("/get").then((response)=>response.json());
 
-  // },[]);
+        switch(jsonGet.result){
+          case "ok":
+            break;
+          case "timeout":
+            console.log("timeout");
+            continue;
+          case "error":
+            throw new Error(jsonGet.message);
+          default:
+            throw new Error("unknown result");
+        }
+        setImage((_previousImage)=>{
+          return jsonGet.data;
+        });
+        
+        const body={
+          data:jsonGet.data,
+        };
+        const jsonSet = await fetch("/set",{
+          method:"POST",
+          headers:{
+            "Content-Type": "application/json",
+          },
+          body:JSON.stringify(body),
+        }).then((response)=>response.json());
+        switch(jsonSet.result){
+          case "ok":
+            console.log("ok");
+            break;
+          default:
+            console.log("unknown result");
+            break;
+        }
+
+      }
+    }
+    main().catch(error=>{
+      console.error(error);
+    });
+  },[]);
   const onGetImage=useCallback((event:MouseEvent<HTMLButtonElement>)=>{
     // event.preventDefault();
     // event.stopPropagation();
